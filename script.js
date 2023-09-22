67ea1808dafe2c6b0328dbbc4bf9211dc7d445aa
Saimoen <gregorysaimoen@outlook.com>
2023-09-22 19:46:21 +1100

    initialisation

---
+++ script.js
@@ -0,0 +1,9 @@
+// On click on « Save », save the value the user just wrote in the corresponding input, in var 
+// « saving »
+
+// - When the user changes a value in a selector, it's value must be saved too
+
+// - The « saved » image that appears at the end of the same… can it's slowly disapear after 2 
+// seconds ?
+
+// - If I save 2 elements at the same time, the whole images animation breaks. Can you fix that 
